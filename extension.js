abcc9030de08d9135d10a90f0abf484888edfb77
Jason Nutter <jasonn@porch.com>
2016-10-10 09:43:05 -0700

    Use promises for git operations

--- extension.js
+++ extension.js
@@ -2,18 +2,18 @@ const vscode = require('vscode');
 const open = require('opn');
 const gitlab = require('gitlab')
 const simpleGit = require('simple-git');
+const Promise = require('bluebird');
 
 function activate(context) {
     const makeMrFromMaster = vscode.commands.registerCommand('extension.makeMrFromMaster', () => {
-        // Get preferences and connect to gitlab
         const preferences = vscode.workspace.getConfiguration('gitlab');
         const gitlabApi = gitlab({
             url: preferences.get('url'),
             token: preferences.get('token')
         });
 
-        // Set git context
         const git = simpleGit(vscode.workspace.rootPath);
+        const gitAsync = Promise.promisifyAll(git);
 
         vscode.window.showInputBox({
             prompt: 'Branch Name:'
@@ -23,28 +23,34 @@ function activate(context) {
             }).then(commit_message => {
                 vscode.window.setStatusBarMessage(`Gitlab: Building MR to master from ${branch}...`);
 
-                git.checkout(['-b', branch])
-                    .add('./*')
-                    .commit(commit_message)
-                    .push(['-u', 'origin', branch])
-                    .getRemotes(true, (err, data) => {
-                        const repo_url = data[0].refs.push;
-                        const repo_id = repo_url.split(":")[1].split(".git")[0];
+                gitAsync.checkoutAsync(['-b', branch])
+                .then(gitAsync.addAsync('./*'))
+                .then(gitAsync.commitAsync(commit_message))
+                .then(gitAsync.pushAsync(['-u', 'origin', branch]))
+                .then(() => {
+                    return gitAsync.getRemotesAsync(true);
+                })
+                .then(remotes => {
+                    const repo_url = remotes[0].refs.push;
+                    const repo_id = repo_url.split(":")[1].split(".git")[0];
 
-                        gitlabApi.projects.merge_requests.add(repo_id, branch, 'master', null, commit_message, data => {
-                            const successMessage = `Gitlab: MR !${data.iid} created.`;
+                    gitlabApi.projects.merge_requests.add(repo_id, branch, 'master', null, commit_message, mr => {
+                        const successMessage = `Gitlab: MR !${mr.iid} created.`;
 
-                            vscode.window.setStatusBarMessage(successMessage);
-                            vscode.window.showInformationMessage(successMessage, 'Open MR').then(selected => {
-                                switch (selected) {
-                                    case 'Open MR': {
-                                        open(data.web_url);
-                                        break;
-                                    }
+                        vscode.window.setStatusBarMessage(successMessage);
+                        vscode.window.showInformationMessage(successMessage, 'Open MR').then(selected => {
+                            switch (selected) {
+                                case 'Open MR': {
+                                    open(mr.web_url);
+                                    break;
                                 }
-                            });
+                            }
                         });
                     });
+                })
+                .catch(err => {
+                    vscode.window.showErrorMessage(err);
+                });
             });
         });
     });
