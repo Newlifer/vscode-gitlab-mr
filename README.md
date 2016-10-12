ab3e911f7a5dc14c6f9d7aed56a101826e19bf2c
Jason Nutter <jasonn@porch.com>
2016-10-12 15:51:15 -0700

    Initial error handling, add comments, improve readme

--- README.md
+++ README.md
@@ -2,21 +2,51 @@
 
 VS Code extension for working with Gitlab Merge Requests.
 
-## Features
+Visual Studio Marketplace: [https://marketplace.visualstudio.com/items?itemName=jasonn-porch.gitlab-mr](https://marketplace.visualstudio.com/items?itemName=jasonn-porch.gitlab-mr)
 
-* MR from master: Opens a MR from uncommitted changes in master.
+## Supported Workflows
+
+### Merge Request from master
+
+This workflow is for creating an MR from uncommitted changes in your `master` branch.
+
+1. Switch to `master` and make changes you want to open an MR for.
+2. Open the command palette and select "Gitlab: MR from master".
+3. First, input the name of the branch you want created for this MR.
+4. Next, provide the commit message for the changes.
+
+At this point, the following will happen:
+1. The new branch will be created and checked out.
+2. All changes files will be committed with the provide commit message.
+3. The branch will be pushed to your Gitlab server.
+4. An MR will be created to `master` for that branch.
+5. A message will be shown in VS Code with a link to the MR.
 
 ## Extension Settings
 
-* `gitlab.url`: url for the Gitlab server.
-* `gitlab.token`: Access token to use to connect to Gitlab API.
+* `gitlab-mr.gitlabUrl`: url for the Gitlab server.
+* `gitlab-mr.accessToken`: Access token to use to connect to Gitlab API. Create one by going to Profile Settings -> Access Tokens.
+* `gitlab-mr.targetBranch`: Default target branch for MRs.
+* `gitlab-mr.targetRemote`: Default target remote for MRs.
 
 ## Known Issues
 
-No error handling yet.
-
 ## Release Notes
+
+### 0.1.0
+
+* Breaking: Changed preferences id from `gitlab` to `gitlab-mr`.
+* Added preference to change default branch name from `master`.
+* Added preference to change default remote repository.
+* Initial error handling for required preferences, commit message, and branch.
+* Updated README with detailed explanation of first workflow.
+* Migrating to public Gitlab.
 
 ### 0.0.1
 
 Initial release. Proof of concept, no error handling.
+
+### Open Source
+
+Repo: [https://gitlab.com/jasonnutter/vscode-gitlab-mr](https://gitlab.com/jasonnutter/vscode-gitlab-mr)
+Issues: [https://gitlab.com/jasonnutter/vscode-gitlab-mr/issues](https://gitlab.com/jasonnutter/vscode-gitlab-mr/issues)
--- extension.js
+++ extension.js
@@ -4,40 +4,76 @@ const gitlab = require('gitlab')
 const simpleGit = require('simple-git');
 const Promise = require('bluebird');
 
-function activate(context) {
-    const makeMrFromMaster = vscode.commands.registerCommand('extension.makeMrFromMaster', () => {
-        const preferences = vscode.workspace.getConfiguration('gitlab');
-        const gitlabApi = gitlab({
-            url: preferences.get('url'),
-            token: preferences.get('token')
-        });
+const STATUS_TIMEOUT = 10000;
 
+const message = message => `Gitlab MR: ${message}`;
+
+exports.activate = context => {
+    const mrFromMaster = vscode.commands.registerCommand('extension.mrFromMaster', () => {
+        // Read and validate preferences
+        const preferences = vscode.workspace.getConfiguration('gitlab-mr');
+        const targetBranch = preferences.get('targetBranch', 'master');
+        const targetRemote = preferences.get('targetRemote', 'origin');
+        const gitlabConfig = {
+            url: preferences.get('gitlabUrl', 'https://gitlab.com'),
+            token: preferences.get('accessToken')
+        };
+
+        if (!gitlabConfig.token) {
+            return vscode.window.showErrorMessage(message('gitlab-mr.accessToken preference not set.'), 'Generate Access Token').then(selected => {
+                switch (selected) {
+                    case 'Generate Access Token':
+                        open(`${gitlabConfig.url}/profile/personal_access_tokens`);
+                        break;
+                }
+            });
+        }
+
+        // Set git and gitlab contexts
+        const gitlabApi = gitlab(gitlabConfig);
         const git = simpleGit(vscode.workspace.rootPath);
         const gitAsync = Promise.promisifyAll(git);
 
+        // Prompt user for branch and commit message
         vscode.window.showInputBox({
             prompt: 'Branch Name:'
         }).then(branch => {
             vscode.window.showInputBox({
                 prompt: 'Commit Messaage:'
             }).then(commit_message => {
-                vscode.window.setStatusBarMessage(`Gitlab: Building MR to master from ${branch}...`);
+                // Validate user inputs
+                if (!branch) {
+                    return vscode.window.showErrorMessage(message('Branch name must be provided.'));
+                }
 
+                if (!commit_message) {
+                    return vscode.window.showErrorMessage(message('Commit message must be provided.'));
+                }
+
+                vscode.window.setStatusBarMessage(message(`Building MR to master from ${branch}...`), STATUS_TIMEOUT);
+
+                // Start git operations
                 gitAsync.checkoutAsync(['-b', branch])
                 .then(gitAsync.addAsync('./*'))
                 .then(gitAsync.commitAsync(commit_message))
-                .then(gitAsync.pushAsync(['-u', 'origin', branch]))
+                .then(gitAsync.pushAsync(['-u', targetRemote, branch]))
                 .then(() => {
                     return gitAsync.getRemotesAsync(true);
                 })
                 .then(remotes => {
+                    if (remotes.length < 1) {
+                        return vscode.window.showErrorMessage(message('No remotes configured.'));
+                    }
+
+                    // Parse Gitlab repo id from remotes
                     const repo_url = remotes[0].refs.push;
                     const repo_id = repo_url.split(":")[1].split(".git")[0];
 
-                    gitlabApi.projects.merge_requests.add(repo_id, branch, 'master', null, commit_message, mr => {
-                        const successMessage = `Gitlab: MR !${mr.iid} created.`;
+                    // Open MR via Gitlab API
+                    gitlabApi.projects.merge_requests.add(repo_id, branch, targetBranch, null, commit_message, mr => {
+                        const successMessage = message(`MR !${mr.iid} created.`);
 
-                        vscode.window.setStatusBarMessage(successMessage);
+                        vscode.window.setStatusBarMessage(successMessage, STATUS_TIMEOUT);
                         vscode.window.showInformationMessage(successMessage, 'Open MR').then(selected => {
                             switch (selected) {
                                 case 'Open MR': {
@@ -47,19 +83,12 @@ function activate(context) {
                             }
                         });
                     });
-                })
-                .catch(err => {
-                    vscode.window.showErrorMessage(err);
                 });
             });
         });
     });
 
-    context.subscriptions.push(makeMrFromMaster);
+    context.subscriptions.push(mrFromMaster);
 }
 
-exports.activate = activate;
-
-function deactivate() { }
-
-exports.deactivate = deactivate;
+exports.deactivate = () => {};
